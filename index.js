98804f0f048ffbb806e1a0ad37b54f3b6075637a
formicae <charly0102@naver.com>
2018-08-09 22:55:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,39 @@ const fs = require('fs');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const app = express();
-const router = express.Router();
-const path = require ('path');
+
 app.set('views', __dirname);
 app.set('view engine', 'html');
 app.engine('html', ejs.renderFile);
 
+app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/loginResource'));
 app.use(bodyParser.urlencoded({extended: false,}));
-const loginRouter = require('./loginResource/login');
 
-app.use('/', loginRouter);
-
-router.get('/example', (req, res, next) => {
-    res.render('example.html',)
-    console.log('second server');
-    next();
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'Dldudah12!',
+    database:'userinfo',
+    port:'3306',
 });
 
-app.listen(3000, () => {
+app.get('/', (req, res, next) => {
+    res.render(__dirname + '/loginResource/login.html');
+});
+app.post('/', (req, res, next) => {
+    const body = req.body;
+    const importData = connection.query('select * from info', (err, data) => {
+        console.log(data);
+        // console.log(allInfo.username, allInfo.userid, allInfo.password);
+    });
+
+});
+
+const signupRouter = require('./loginResource/signup');
+app.use('/', signupRouter);
+
+
+app.listen(3500, () => {
     console.log('server is running !');
 });

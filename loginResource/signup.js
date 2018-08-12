9128307cdc49cdfd3b2f7661d24913168425d20d
formicae <charly0102@naver.com>
2018-08-12 11:52:20 +0900

    Update signup.js

--- loginResource/signup.js
+++ loginResource/signup.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
 const mysql = require('mysql');
 const app = express();
+const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({
-    extended: false,
-}));
-
+const cookiePaser = require('cookie-parser');
+const SECRET_KEY = 'secret';
+app.use(bodyParser.urlencoded({ extended: false, }));
+app.use(cookiePaser());
 const connection = mysql.createConnection({
     host:'localhost',
     user:'root',
@@ -17,14 +17,23 @@ const connection = mysql.createConnection({
 });
 
 router.get('/signup', (req, res, next) => {
-    res.render('loginResource/signup', {title:"Index file haha @"});
+    res.render('loginResource/signup.html', {title:"Index file haha @"});
 });
+
 router.post('/signup', (req, res, next) => {
-    const body = req.body;
-    const value = [body.username, body.userid, body.password, body.pwhintquestion, body.pwhintanswer]
-    console.log(value);
-    connection.query('INSERT INTO info VALUES (?,?,?,?,?)', [value], (err, results, fields) => {
-        console.log('Data added !!');
+    const bodyData = {"userid":req.body.userid,"password":req.body.password};
+    const token = jwt.sign(bodyData, SECRET_KEY, {algorithm: 'HS256', expiresIn: '10m'});
+    connection.query("INSERT INTO info(username, userid, password, token) VALUES ('"+req.body.username+"','"+req.body.userid+"','"+req.body.password+"','"+token+"')", (err, results, fields) => {
+        if (err) throw err;
+        console.log('data add to MySQL done!');
+        res.cookie(req.body.userid,token);
+        res.redirect('/');
     });
 });
+
+router.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send('Error occured while updating MySQL database !');
+});
+
 module.exports = router;

57e04b7e74093b7493847d59f613ab20bb8e95f7
formicae <charly0102@naver.com>
2018-08-12 15:49:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
-const fs = require('fs');
 const ejs = require('ejs');
-const jwt = require('jsonwebtoken');
+const session = require('express-session');
 const cookiePaser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const app = express();
@@ -19,73 +18,57 @@ app.set('view engine', 'html');
 app.engine('html', ejs.renderFile);
 
 app.use(cookiePaser());
+app.use(session({
+    key:'youngmo',
+    secret:'mo',
+    resave:true,
+    saveUninitialized:true,
+}));
 app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/loginResource'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false,}));
 
+const signupRouter = require('./loginResource/signup');
+const userpageRouter = require('./loginResource/userpage');
+app.use('/', signupRouter);
+app.use('/', userpageRouter);
 let visited = 0;
-app.get('/', (req, res, next) => {
-    console.log(req.cookies);
-    const allCookieName = Object.keys(JSON.parse(JSON.stringify(req.cookies)));
-    if (visited === 0) {
-        for (let i = 0; i < allCookieName.length; i++) {
-            res.cookie(allCookieName[i], 'initial-cookie');
-        }
-    }
-    visited += 1;
-    fs.readFile('loginResource/login.html', 'utf-8', (err, data) => {
-        if (err) next(err);
-        res.send(ejs.render(data, {text: `welcome! you visited here ${visited} times!`}));
-    });
-});
 
-app.get('/user/:id', (req, res, next) => {
-    const transferredId = req.params.id;
-    const browserCookie = req.cookies[transferredId];
-    connection.query("select username, token from info where userid = ?", [transferredId], (err, result) => {
-        if (err) next (err);
-        if (browserCookie === result[0].token) {
-            res.sendFile(__dirname+'/loginResource/userpage.html',{userid:transferredId});
-            //// send html file for log out -> if log out, cookie must be re-initialized.
-        } else {
-            console.log('Wrong cookie @@@@@@ ');
-            res.redirect('/');
-        }
-    });
+app.get('/', (req, res, next) => {
+    console.log('main session : ',req.session.prevurl);
+    if ((visited > 0 && !req.session.prevurl) || req.session.prevurl === '/loggout') {
+        req.session.prevurl = '/';
+        res.render('loginResource/login.html',{text: `Log out !! `});
+    } else {
+        req.session.prevurl = '/';
+        visited += 1;
+        res.render('loginResource/login.html',{text: `welcome! you visited here ${visited} times!`})
+    }
 });
 
 app.post('/', (req, res, next) => {
     const body = req.body;
-    const bodyData = {"userid":body.userid,"password":body.password};
     connection.query('select username, userid, password, token from info where userid = ?', body.userid, (err, result) => {
         if (err) { next(err); }
         else {
             if (result.length === 0) {
-                console.log('No corresponding User ID ! check again.');
+                res.render('loginResource/login.html',{text: 'No corresponding User ID ! check again.'})
             } else {
                 if (body.password === result[0].password) {
-                    // const browserToken = req.cookies[body.userid];
-                    // console.log('MySQL token : ',token);
-                    // console.log('browser token : ', browserToken);
+                    res.cookie(body.userid, result[0].token);
                     res.redirect('/user/' + body.userid);
-                } else { console.log(`Wrong password ${body.userid} !`); }
+                } else {
+                    res.render('loginResource/login.html',{text: `Wrong password ${body.userid} !`})
+                }
             }
         }
     });
 });
 
-const signupRouter = require('./loginResource/signup');
-app.use('/', signupRouter);
-
 app.use((err, req, res, next) => {
     console.log(err);
     res.status(500).send('Error occured while reading login.html!');
-});
-
-app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).send('Error occured while loading user page !');
 });
 
 app.use((err, req, res, next) => {
